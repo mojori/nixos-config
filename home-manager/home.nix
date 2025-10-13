64d1be0740748bb2969f420e76741d9db0c90e9e
katharye <multiden007@gmail.com>
2025-10-14 00:15:42 +0300

    add zsh config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,23 +7,7 @@
 	
   imports = [
     ./modules
+	./zsh.nix
   ];
 
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			btw = "echo i suck big dicks btw";
-			".." = "cd ..";
-			nrs = "sudo nixos-rebuild switch";
-			hms = "home-manager switch --flake /home/katharye/nix/";
-		};
-		
-		initExtra = ''
-			export PS1='\[\e[38;5;176;3m\]\u\[\e[0m\] in \[\e[1m\]\w\[\e[0m\] \\$ '
-		'';
-	};
-
-	home.packages = with pkgs; [
-		bat
-	];
 }
